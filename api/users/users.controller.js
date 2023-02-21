282e8697c0ccb3771f7662c6f86304ffaf8d51d3
Swapnil <swapnilbendal155@gmail.com>
2023-02-21 15:45:42 +0530

    update email

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -267,6 +267,7 @@ module.exports = {
 
 
                 const result = sendEmailOTP(email, otp)
+                console.log(result)
                 return res.status(200).json({
                     message: `Successfully resend a OTP`,
                     user: updateOtp
