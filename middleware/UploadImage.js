ea1c170fa16204b40a5f63bd7a7bb194815c523b
Swapnil <swapnilbendal155@gmail.com>
2023-02-24 19:00:30 +0530

    upload 11

--- middleware/UploadImage.js
+++ middleware/UploadImage.js
@@ -44,7 +44,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        const dir = "/Uploads"
+        const dir = "/UploadImage"
 
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir)
