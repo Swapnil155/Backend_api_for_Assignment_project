1361a3991add1497dd50f758bb3c9d2078937a29
Swapnil <swapnilbendal155@gmail.com>
2023-02-24 15:19:40 +0530

    change 24

--- middleware/UploadImage.js
+++ middleware/UploadImage.js
@@ -2,8 +2,27 @@ const multer = require('multer')
 const fs = require('fs')
 const path = require('path')
 
+// const storage = multer.diskStorage({
+//     destination : (req, files, callback) => {
+//         const dir = "Uploads"
+
+//         // if (!fs.existsSync(dir)) {
+//         //     fs.mkdirSync(dir)
+//         // }
+
+//         callback(null, dir)
+//     },
+//     filename : (req, files, callback)=>{
+//         callback(null, files.fieldname+'-'+Date.now()+ path.extname(files.originalname))
+//         console.log(files)
+//     },
+//     // limits : {
+//     //     fileSize : 100*100
+//     // }
+// })
+
 const storage = multer.diskStorage({
-    destination : (req, files, callback) => {
+    destination: (req, file, callback) => {
         const dir = "Uploads"
 
         if (!fs.existsSync(dir)) {
@@ -12,9 +31,9 @@ const storage = multer.diskStorage({
 
         callback(null, dir)
     },
-    filename : (req, files, callback)=>{
-        callback(null, files.fieldname+'-'+Date.now()+ path.extname(files.originalname))
-        console.log(files)
+    filename: (req, file, callback) => {
+        callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        console.log(file)
     },
     // limits : {
     //     fileSize : 100*100
